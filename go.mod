06df8b9ccc8c6265e5cf3b0a8b5d2ce45aea9364
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-01-10 23:33:25 +0100

    WIP

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-axfr-backend
 
 go 1.19
 
-require github.com/go-sql-driver/mysql v1.7.0
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
 )
 
-func dbConn(dbName string) (db *sql.DB) {
+func dbConn(dbName string, dbUser string, dbPass string) (db *sql.DB) {
 	dbDriver := "mysql"
-	dbUser := os.Getenv("MYSQL_USERNAME")
-	dbPass := os.Getenv("MYSQL_PASSWORD")
+	//dbUser := dbName //os.Getenv("MYSQL_USERNAME")
+	//dbPass := dbName //os.Getenv("MYSQL_PASSWORD")
 	MYSQL_HOSTNAME := os.Getenv("MYSQL_HOSTNAME")
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp"+"("+MYSQL_HOSTNAME+")"+"/"+dbName)
 	if err != nil {
@@ -21,40 +24,67 @@ func dbConn(dbName string) (db *sql.DB) {
 	return db
 }
 
-func sendSeDates(pageordate int) {
-	db := dbConn("sediff")
-	if pageordate < 8 {
-		var rows1 = pageordate * 20
-		var rows2 = 0
-		if pageordate == 0 {
-			rows2 = 0
-		} else {
-			rows2 = rows1
-		}
-		rows, err := db.Query("SELECT date, amount FROM dates ORDER BY date DESC OFFSET ? ROWS FETCH FIRST 20 ROWS ONLY", rows2)
+func sendDates(diffdb string, dbUser string, dbPass string, pageordate int) []byte {
+	db := dbConn(diffdb, dbUser, dbPass)
+	/* if pageordate < 8 { */
+
+	var rows1 = pageordate * 20
+	var rows2 int
+	if pageordate == 0 {
+		rows2 = 0
+	} else {
+		rows2 = rows1
+	}
+	fmt.Println(pageordate)
+	fmt.Println(rows2)
+	rows, err := db.Query("SELECT date, amount FROM dates ORDER BY date DESC OFFSET ? ROWS FETCH FIRST 20 ROWS ONLY", rows2)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	type Amounts struct {
+		Date   int `json:"date"`
+		Amount int `json:"amount"`
+	}
+	var arr []Amounts
+	for rows.Next() {
+		var date int
+		var amount int
+		rows.Scan(&date, &amount)
+		a := Amounts{Date: date, Amount: amount}
+		arr = append(arr, a)
+	}
+	j, _ := json.Marshal(arr)
+	//fmt.Print(string(j))
+	return j
+	/* } */ /* else {
+		rows, err := db.Query("SELECT date, amount FROM dates ORDER BY date DESC")
 		if err != nil {
 			panic(err.Error())
 		}
-		defer rows.Close()
+		lol := fmt.Println(rows)
+		return lol
+	} */
+	//return "asd"
+}
 
-		var arr []string
-		for rows.Next() {
-			var date int
-			var amount int
-			rows.Scan(&date, &amount)
-			mapD := map[string]int{"date": date, "amount": amount}
-			mapB, _ := json.Marshal(mapD)
-			//fmt.Println(string(mapB))
-			arr = append(arr, string(mapB))
-		}
-		fmt.Print(arr)
-	} else {
-
+func httpSendDates(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil {
+		panic(err.Error())
 	}
+	result := sendDates(os.Getenv("MYSQL_SE_DATABASE"), os.Getenv("MYSQL_SE_USERNAME"), os.Getenv("MYSQL_SE_PASSWORD"), id)
+	w.Write(result)
 }
 
 func main() {
+	router := httprouter.New()
 
-	sendSeDates(1)
+	router.GET("/se/:id", httpSendDates)
 
+	//fmt.Println(sendDates(os.Getenv("MYSQL_SE_DATABASE"), os.Getenv("MYSQL_SE_USERNAME"), os.Getenv("MYSQL_SE_PASSWORD"), 0))
+	//fmt.Println(sendDates(os.Getenv("MYSQL_NU_DATABASE"), os.Getenv("MYSQL_NU_USERNAME"), os.Getenv("MYSQL_NU_PASSWORD"), 1))
+
+	http.ListenAndServe(":8080", router)
 }
