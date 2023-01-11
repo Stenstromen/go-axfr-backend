02404489f1d7a4e8a81db8b4e1d897d7f0824360
Stenstromen <kontakt@filipstenstrom.se>
2023-01-11 21:36:30 +0100

    WIP

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"os"
 	"strconv"
@@ -14,8 +13,6 @@ import (
 
 func dbConn(dbName string, dbUser string, dbPass string) (db *sql.DB) {
 	dbDriver := "mysql"
-	//dbUser := dbName //os.Getenv("MYSQL_USERNAME")
-	//dbPass := dbName //os.Getenv("MYSQL_PASSWORD")
 	MYSQL_HOSTNAME := os.Getenv("MYSQL_HOSTNAME")
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp"+"("+MYSQL_HOSTNAME+")"+"/"+dbName)
 	if err != nil {
@@ -26,7 +23,6 @@ func dbConn(dbName string, dbUser string, dbPass string) (db *sql.DB) {
 
 func sendDates(diffdb string, dbUser string, dbPass string, pageordate int) []byte {
 	db := dbConn(diffdb, dbUser, dbPass)
-	/* if pageordate < 8 { */
 
 	var rows1 = pageordate * 20
 	var rows2 int
@@ -35,8 +31,6 @@ func sendDates(diffdb string, dbUser string, dbPass string, pageordate int) []by
 	} else {
 		rows2 = rows1
 	}
-	fmt.Println(pageordate)
-	fmt.Println(rows2)
 	rows, err := db.Query("SELECT date, amount FROM dates ORDER BY date DESC OFFSET ? ROWS FETCH FIRST 20 ROWS ONLY", rows2)
 	if err != nil {
 		panic(err.Error())
@@ -56,35 +50,29 @@ func sendDates(diffdb string, dbUser string, dbPass string, pageordate int) []by
 		arr = append(arr, a)
 	}
 	j, _ := json.Marshal(arr)
-	//fmt.Print(string(j))
 	return j
-	/* } */ /* else {
-		rows, err := db.Query("SELECT date, amount FROM dates ORDER BY date DESC")
-		if err != nil {
-			panic(err.Error())
-		}
-		lol := fmt.Println(rows)
-		return lol
-	} */
-	//return "asd"
-}
-
-func httpSendDates(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	id, err := strconv.Atoi(ps.ByName("id"))
-	if err != nil {
-		panic(err.Error())
-	}
-	result := sendDates(os.Getenv("MYSQL_SE_DATABASE"), os.Getenv("MYSQL_SE_USERNAME"), os.Getenv("MYSQL_SE_PASSWORD"), id)
-	w.Write(result)
 }
 
 func main() {
 	router := httprouter.New()
 
-	router.GET("/se/:id", httpSendDates)
+	router.GET("/se/:id", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		id, err := strconv.Atoi(ps.ByName("id"))
+		if err != nil {
+			panic(err.Error())
+		}
+		result := sendDates(os.Getenv("MYSQL_SE_DATABASE"), os.Getenv("MYSQL_SE_USERNAME"), os.Getenv("MYSQL_SE_PASSWORD"), id)
+		w.Write(result)
+	})
 
-	//fmt.Println(sendDates(os.Getenv("MYSQL_SE_DATABASE"), os.Getenv("MYSQL_SE_USERNAME"), os.Getenv("MYSQL_SE_PASSWORD"), 0))
-	//fmt.Println(sendDates(os.Getenv("MYSQL_NU_DATABASE"), os.Getenv("MYSQL_NU_USERNAME"), os.Getenv("MYSQL_NU_PASSWORD"), 1))
+	router.GET("/nu/:id", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		id, err := strconv.Atoi(ps.ByName("id"))
+		if err != nil {
+			panic(err.Error())
+		}
+		result := sendDates(os.Getenv("MYSQL_NU_DATABASE"), os.Getenv("MYSQL_NU_USERNAME"), os.Getenv("MYSQL_NY_PASSWORD"), id)
+		w.Write(result)
+	})
 
 	http.ListenAndServe(":8080", router)
 }
