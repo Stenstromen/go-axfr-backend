81bbd2ee49a3e63a7b24a30f04ac0844f91ae585
Stenstromen <kontakt@filipstenstrom.se>
2025-03-16 09:28:22 +0100

    Updated to go 1.24 and removed http header

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -7,12 +7,6 @@ import (
 func SetupRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("OPTIONS /", func(w http.ResponseWriter, r *http.Request) {
-		header := w.Header()
-		header.Set("Access-Control-Allow-Methods", header.Get("Allow"))
-		w.WriteHeader(http.StatusNoContent)
-	})
-
 	mux.HandleFunc("/ready", readyness)
 	mux.HandleFunc("/status", liveness)
 	mux.HandleFunc("/se/", Middleware(sendSEDates))
